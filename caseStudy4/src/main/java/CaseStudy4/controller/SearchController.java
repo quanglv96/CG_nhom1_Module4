06a27e0ab1f9116a753eda5cc530ca66c2e1f9f1
truong-ngo <truong.ngo1993@gmail.com>
2023-01-06 10:13:52 +0700

    update css file

--- caseStudy4/src/main/java/CaseStudy4/controller/SearchController.java
+++ caseStudy4/src/main/java/CaseStudy4/controller/SearchController.java
@@ -1,5 +1,8 @@
 package CaseStudy4.controller;
 
+import CaseStudy4.model.Playlist;
+import CaseStudy4.model.Songs;
+import CaseStudy4.model.Users;
 import CaseStudy4.service.Songs.ISongService;
 import CaseStudy4.service.playlist.IPlaylistService;
 import CaseStudy4.service.users.IUserService;
@@ -25,22 +28,22 @@ public class SearchController {
 
     @GetMapping
     public ResponseEntity<Object> search(@RequestParam("search") String text){
-        List resultSearch=new ArrayList<>();
+        List<Object> resultSearch=new ArrayList<>();
         resultSearch.add(findSongByName(text));
         resultSearch.add(findPlaylistByName(text));
         resultSearch.add(findUserByName(text));
         return new ResponseEntity<>(resultSearch,HttpStatus.OK);
     }
     @GetMapping("/songs")
-    public Iterable findSongByName(@RequestParam("search") String text){
+    public Iterable<Songs> findSongByName(@RequestParam("search") String text){
         return iSongService.findAllByNameContaining(text);
     }
     @GetMapping("/playlist")
-    public Iterable findPlaylistByName(@RequestParam("search") String text){
+    public Iterable<Playlist> findPlaylistByName(@RequestParam("search") String text){
         return iPlaylistService.findAllByNameContaining(text);
     }
     @GetMapping("/user")
-    public Iterable findUserByName(@RequestParam("search") String text){
+    public Iterable<Users> findUserByName(@RequestParam("search") String text){
         return iUserService.findAllByNameContaining(text);
     }
 
