b2d26763c04604914a458c2324ac2bf39a9f10c2
truong-ngo <truong.ngo1993@gmail.com>
2023-01-08 23:07:29 +0700

    sửa modal html css js và thêm validator.js

--- caseStudy4/views/discovery/assets/js/modal.js
+++ caseStudy4/views/discovery/assets/js/modal.js
@@ -1,55 +1,81 @@
-// function openUpload() {
-//     $(".input-file").click();
-// }
-//
-// function replaceImg(myImg) {
-//     file = myImg.files[0]
-//     let path = (window.URL || window.webkitURL).createObjectURL(file);
-//     $(".modal-img").attr("src", path);
-// }
-//
-// function openUploadMp3(callback) {
-//     $(".input-mp3-file").click();
-// }
-//
-// function getFileName(myFile) {
-//     let file = myFile.files[0];
-//     let fileName = file.name;
-//     $(".mp3-name").text(fileName);
-// }
-//
-// function openModal() {
-//     $(".modal").show();
-// }
-//
-// function closeModal() {
-//     $(".modal").hide();
-// }
-//
-// window.onclick = function(event) {
-//     if (!event.target.closest(".modal-dialogue,.modal-open")) {
-//         $(".modal").hide();
-//     }
-// }
-
-// Toggle login modal
-function openLoginModal() {
-    $(".login-modal").show();
-}
-
+// Open - close modal
 window.onclick = function(event) {
     if (!event.target.closest(".login-modal-dialogue,.login-btn")) {
         $(".login-modal").hide();
     }
+    if (!event.target.closest(".song-modal-dialogue,.song-btn")) {
+        $(".song-modal").hide();
+    }
+    if (!event.target.closest(".playlist-modal-dialogue,.playlist-btn")) {
+        $(".playlist-modal").hide();
+    }
 }
 
+let songForm = document.querySelector(".song-form")
+songForm.onsubmit = function(e) {
+    e.preventDefault();
+}
+
+let playlistForm = document.querySelector(".playlist-form")
+playlistForm.onsubmit = function(e) {
+    e.preventDefault();
+}
+
+function openModal(selector) {
+    $(selector).show();
+}
+
+function closeModal() {
+    $(".modal").hide();
+}
+
+function openUpload(selector) {
+    $(selector).click();
+}
+
+function replaceImg(myImg, selector) {
+    let file = myImg.files[0]
+    let path = (window.URL || window.webkitURL).createObjectURL(file);
+    $(selector).attr("src", path);
+}
+
+function getFileName(myFile) {
+    let file = myFile.files[0];
+    let fileName = file.name;
+    $(".mp3-name").text(fileName);
+}
+
+// Login modal
 function switchTab(other, active) {
     if ($(".modal-tab-" + other).hasClass("hide")) {
-        $(".modal-tab-" + other).removeClass("hide")
-        $(".modal-tab-" + other).addClass("show")
-        $(".modal-tab-" + active).removeClass("show")
-        $(".modal-tab-" + active).addClass("hide")
+        $(".modal-tab-" + other).removeClass("hide").addClass("show")
+        $(".modal-tab-" + active).removeClass("show").addClass("hide")
         $("." + other).css("background-color", "white")
         $("." + active).css("background-color", "#cccccc")
     }
 }
+
+validator({
+    form: "#register-form",
+    errorSelector: ".form-message",
+    rules: [
+        validator.isRequired("#c-username"),
+        validator.isRequired("#c-password"),
+        validator.lengthField("#c-password", 6, 8),
+        validator.isRequired("#re-password"),
+        validator.isConfirmed("#re-password", function () {
+            return document.querySelector("#register-form #c-password").value;
+        }),
+        validator.isRequired("#phone-number")
+    ]
+})
+
+validator({
+    form: "#login-form",
+    errorSelector: ".form-message",
+    rules: [
+        validator.isRequired("#username"),
+        validator.isRequired("#password"),
+        validator.lengthField("#password", 6, 8)
+    ],
+})
