08c8cc26d1bd3ae27ddc4f2b1dd2e2631c6666c5
KhucSongNgan1984 <khucsongngan@gmail.com>
2023-01-09 13:43:22 +0700

    CRUD Tags

--- caseStudy4/src/main/java/CaseStudy4/repository/ITagRepository.java
+++ caseStudy4/src/main/java/CaseStudy4/repository/ITagRepository.java
@@ -2,6 +2,8 @@ package CaseStudy4.repository;
 
 import CaseStudy4.model.Tags;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ITagRepository extends JpaRepository<Tags, Long> {
 }
---
+++ caseStudy4/src/main/java/CaseStudy4/service/Tags/ITagService.java
@@ -0,0 +1,9 @@
+package CaseStudy4.service.Tags;
+
+import CaseStudy4.model.Songs;
+import CaseStudy4.model.Tags;
+import CaseStudy4.service.IGeneralService;
+import org.springframework.stereotype.Service;
+
+public interface ITagService extends IGeneralService <Tags> {
+}
