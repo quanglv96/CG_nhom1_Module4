e7da71c135a54225ac40135634e2e0ad556f35e4
quanglv96 <quanglv1712@gmail.com>
2023-01-09 08:52:48 +0700

    edit header home.html

--- caseStudy4/views/home/js/home_JS.js
+++ caseStudy4/views/home/js/home_JS.js
@@ -41,7 +41,19 @@ function redirectResultSearchUser(idUser) {
     localStorage.setItem("searchIdUser", idUser);
     window.location = "../../discovery/layout/layout.html";
 }
+let slideIndex = 0;
+slideShow();
 
+function slideShow() {
+    let slides = document.getElementsByClassName("banner-item");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(slideShow, 2000); // Change image every 2 seconds
+}
 
 
 
